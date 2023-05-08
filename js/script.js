e86ddc61f43414f881626cea579260f0816f7ca9
casey_l <clawver11@gmail.com>
2023-05-08 19:16:55 -0400

    Event listeners

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// Your JS code here
+var subTypeElement = document.querySelector(".subscription-select");
+var subDurationElement = document.querySelector(".duration-select");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = Number(1);
+
+subTypeElement.addEventListener("change", function (e) {
+    subType = e.target.value;
+});
+
+subDurationElement.addEventListener("change", function(e){
+subDuration = Number(e.target.value);
+console.log(subDuration);
+});
