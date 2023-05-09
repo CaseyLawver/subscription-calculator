528cc3ce2dd999e0b6b5ba483688bf61718b5bbe
casey_l <clawver11@gmail.com>
2023-05-08 20:22:18 -0400

    Finished calculator

--- js/script.js
+++ js/script.js
@@ -6,9 +6,21 @@ var subDuration = Number(1);
 
 subTypeElement.addEventListener("change", function (e) {
     subType = e.target.value;
+    updateSubscriptionDiv();
 });
 
 subDurationElement.addEventListener("change", function(e){
 subDuration = Number(e.target.value);
-console.log(subDuration);
+updateSubscriptionDiv();
 });
+
+var updateSubscriptionDiv = function () {
+    var monthlyCost = Number(5);
+if (subType === "standard") {
+   monthlyCost = Number(7);
+} else if (subType === "premium") {
+    monthyCost = Number(10);
+}
+var total = subDuration * monthlyCost;
+result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}`;
+};
